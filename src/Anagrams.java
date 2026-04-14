89ee7f3d98c67a9583c13a91e54656fefd3a8ce8
Akash <akashpoddar492@gmail.com>
2026-04-14 18:30:19 +0530

    check is anagram or not

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -4,8 +4,32 @@ public class Anagrams {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String");
-        String str = sc.nextLine();
+        String s1 = sc.nextLine();
+        System.out.println("Enter a String");
+        String s2 = sc.nextLine();
+        System.out.println("isAnagram(s1,s2) = " + isAnagram(s1, s2));
 
+
+    }
+
+    static Boolean isAnagram(String s1 , String s2){
+        if (s1.length() != s2.length()) return false;
+
+        int[] freq = new int[26];
+
+        for (char ch : s1.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : s2.toCharArray()) {
+            freq[ch - 'a']--;
+        }
+
+        for (int num : freq) {
+            if (num != 0) return false;
+        }
+
+        return true;
 
     }
 }
