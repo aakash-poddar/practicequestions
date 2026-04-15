7230eb01690dc65cde13c547b1c5cdd7fa6e70cf
Akash <akashpoddar492@gmail.com>
2026-04-15 18:43:43 +0530

    multiplication table

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+
+        multiplicationTable(num);
+    }
+
+     static void multiplicationTable(int num) {
+
+         for (int i = 0; i <= 10; i++) {
+             System.out.println(num + " * " + i + " = " + num*i);
+
+         }
+    }
+
+}
+
